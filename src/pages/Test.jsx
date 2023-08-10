dd4de0c1b0c126fafa65d14093620de178d3b299
camivand4 <camivand4@student.arteveldehs.be>
2023-08-10 15:41:59 +0200

    test api

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,40 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useLoaderData } from "react-router-dom";
-import { dataFetcher } from '../components/DataFetcher.js';
 
-  export const Test = () => {
-    const [data, setData] = useState([]);
-    const [statusCode, setStatusCode] = useState(0);
-    const [errorMessage, setErrorMessage] = useState('');
-  
-    useEffect(() => {
-      const fetchData = async () => {
-        const result = await dataFetcher();
-        setData(result.data);
-        setStatusCode(result.status);
-        setErrorMessage(result.message);
-      };
-      fetchData();
-    }, []);
-  
+export const dataFetcher = async () => {
+    const rest = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+    const data = await rest.json();
+    return data;
+}
+
+export const Test = () => {
+    const data = useLoaderData();
     return (
-      <div>
-        {statusCode === 200 ? (
-          data.map((device) => (
-            <div key={device.id}>
-              <p>Name: {device.name}</p>
-              <p>Plant: {device.plant}</p>
-              <p>Humidity: {device.humidity}</p>
-              <p>Watering Time: {device.wateringTime}</p>
-              {/* Add other device properties as needed */}
-            </div>
-          ))
-        ) : (
-          <p>Error occurred. Status Code: {statusCode}</p>
-        )}
-  
-        {errorMessage && <p>Error Message: {errorMessage}</p>}
-      </div>
-    );
-  };
-  
+        <div>{data.title}</div>
+    )
+}
